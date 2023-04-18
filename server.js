f5058cf46df07e2acf94ab3f5626e18ed9d69585
Victoria Benoit <vrose834@gmail.com>
2023-04-18 14:41:18 -0300

    Finished the server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || env;
 
-app.use(routes);
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(routes);
 
 
 sequelize.sync({force: false}).then(() => {
