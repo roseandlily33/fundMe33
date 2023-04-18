acb637fc0891b16310133c5b42f7e09832969e16
Victoria Benoit <vrose834@gmail.com>
2023-04-18 20:13:53 -0300

    Added more of the templates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,4 +15,12 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+})
+
 module.exports = router;
